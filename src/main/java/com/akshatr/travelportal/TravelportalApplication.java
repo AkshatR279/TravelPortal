4c5763954b75eecab20f7b79dc17523d252e1e3e
AkshatR279 <akshatr279.b@gmail.com>
2026-07-23 00:03:35 +0530

    [23/07/2026] - Intitial Commit

---
+++ src/main/java/com/akshatr/travelportal/TravelportalApplication.java
@@ -0,0 +1,13 @@
+package com.akshatr.travelportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelportalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akshatr/travelportal/TravelportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshatr.travelportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
